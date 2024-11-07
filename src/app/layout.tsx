5bd02c0ec6b93b0777d9f3a237f9e9e8342c0590
ltphen <tatkeulionel@gmail.com>
2024-11-08 00:11:15 +0100

    changed default theme to ligh mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         <body className={font.className}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
