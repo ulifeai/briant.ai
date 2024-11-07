42e4347050b208542b8f8b98c1f76f00d41036f9
ltphen <tatkeulionel@gmail.com>
2024-11-07 21:56:33 +0100

    changed model to azure

--- src/lib/ai/model.ts
+++ src/lib/ai/model.ts
@@ -1,6 +1,6 @@
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatOpenAI, AzureChatOpenAI } from "@langchain/openai";
 
-export const Model = new ChatOpenAI({
+export const Model = new AzureChatOpenAI({
     model: "gpt-4o",
     temperature: 1
 });
